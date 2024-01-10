20d867dcc63e5699ecf7e71362461a8e260d08d4
Stephan Kansy <stkansy@gmail.com>
2024-01-10 09:02:37 +0100

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using AJAXExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AJAXExample.Controllers
+{
+    public class CustomerController : Controller
+    {
+        List<CustomerModel> customers = new List<CustomerModel>();
+
+        public CustomerController()
+        {
+            customers.Add(new CustomerModel { Id = 0, Name = "Sherry", Age = 42});
+            customers.Add(new CustomerModel { Id = 1, Name = "Melvin", Age = 18 });
+            customers.Add(new CustomerModel { Id = 2, Name = "Velma", Age = 24 });
+            customers.Add(new CustomerModel { Id = 3, Name = "Wendy", Age = 78 });
+            customers.Add(new CustomerModel { Id = 4, Name = "Kim", Age = 20 });
+            customers.Add(new CustomerModel { Id = 5, Name = "Jerry", Age = 35 });
+        }
+        public IActionResult Index()
+        {
+            return View(customers);
+        }
+
+        public IActionResult ShowOnePerson(int Id)
+        {
+            CustomerModel c = customers.FirstOrDefault(x => x.Id == Id);
+            // es ist wichtig, dass die view auf eine partielle view geänder wird (c), da sonst das ajax die partielle anzeige nicht
+            // anzeigen wird, sondern die standard view
+            return View(c);
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(function () {
+    console.log("Page is ready!");
+
+    // durch diesen Teil wird der RadioButton ein OneClickEvent (+IndexTeil)
+    $(".customerRadio").change(function () {
+        console.log("Radio button changed");
+        doCustomerUpdate();
+    });
+
+    function doCustomerUpdate() {
+        $.ajax({
+            type: "POST",
+            url: 'Customer/ShowOnePerson',
+            // formatiert die seite
+            data: $("form").serialize(),
+            success: function (data) {
+                console.log(data);
+                $("#customerInformationArea").html(data);
+            }
+        });
+    };
+
+    $("#selectCustomer").click(function () {
+        event.preventDefault();
+        console.log("Kontrollnachricht: durch 'event.preventDefault();' wird das standard event des submit buttons geblockt.");
+        doCustomerUpdate();
+    });
+});
